b24064a97f122e5acc1601dca571cb66eb8c7401
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-05-25 22:37:04 +0300

    ++

--- src/components/Friends/FriendsList.js
+++ src/components/Friends/FriendsList.js
@@ -6,6 +6,7 @@ import styles from './FriendsList.module.css'
 export default function FriendsList ({friends}) {
   return (
     <ul className={styles.friendsList}>
+      <h3 style={{ margin:"0 0 20px" }}>Friends</h3>
         {friends.map(({ id, avatar, name, isOnline }) => (
             <FriendsListItem
                 key={id}
